99fbc52dee21a11478aa25325f4bb10c8fea8a43
Raza Jaffery <110700927+razaj96@users.noreply.github.com>
2022-12-24 02:06:03 -0800

    added basic starting files with first challenge and solution

---
+++ index.js
@@ -0,0 +1,25 @@
+// problem 1
+const today = new Date();
+
+const day = today.getDay();
+const month = today.getMonth();
+const year = today.getFullYear();
+const daylist = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+//need to fix this to get AM or PM
+function getDate() {
+  document.getElementById("current-day").innerHTML = `Today is ${daylist[day]}`;
+  document.getElementById(
+    "current-date"
+  ).innerHTML = `Current time is : ${today.getHours()} : ${today.getMinutes()} : ${today.getSeconds()}`;
+}
+
+getDate();
